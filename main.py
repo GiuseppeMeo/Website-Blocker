0c197b0af3265d3eb5429130b1fdbfe6698da0f0
Giuseppe Meo <48062283+GiuseppeMeo@users.noreply.github.com>
2022-03-14 03:27:30 +0000

    create main.py
    Code for website blocker final

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+from datetime import datetime as dt
+
+hosts_path = "/etc/hosts"
+# localhost's IP
+redirect = "127.0.0.1"
+
+# websites to block
+website_list = ["www.facebook.com", "facebook.com", "www.instagram.com", "instagram.com", "www.twitter.com", "twitter.com"]
+
+while True:
+
+    # time of your work
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8)< dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print("Working hours...")
+    with open(hosts_path, 'r+') as file:
+        content = file.read()
+        for website in website_list:
+            if website in content:
+                pass
+            else:
+                file.write(redirect + " " + website + "\n")
+        else:
+            with open(hosts_path, 'r+') as file:
+                content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+
+            # removing hostnmes from host file
+            file.truncate()
+
+        print("Fun hours...")
+        time.sleep(5)
